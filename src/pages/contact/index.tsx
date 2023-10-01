6db2afcfaf33467e53f533f786d87f4ff48d4d93
s3nmith <lahiruudawatta@gmail.com>
2023-10-01 04:43:27 +0400

    contact update

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import HeaderCard from "../../components/HeaderCard";
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
+import styles from "./contact.module.css";
 
 const ContactPage: React.FC = () => {
   return (
@@ -16,6 +17,15 @@ const ContactPage: React.FC = () => {
         subtitle="Get in touch with our dedicated team"
         backgroundImage="/img/head/hea.png"
       />
+      <div className={styles.FrameBox}>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfFGRmLNUhKn0DvFdSjl7G6NN6wJEsUQj4TKkPxmZWjFwEgwg/viewform?embedded=true"
+          width="640"
+          height="688"
+        >
+          Loading…
+        </iframe>
+      </div>
       <Footer />
     </div>
   );
